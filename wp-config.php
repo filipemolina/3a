e1d92c72e6e5472e1923a509bd5b7fe6418e56da
Filipe Molina <filipemolina@live.com>
2015-01-05 15:32:00 -0200

    Primeiro Commit

---
+++ wp-config.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, and ABSPATH. You can find more information by visiting
+ * {@link http://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
+ * Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', '3aworldwide');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', '');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'f>R9t}icufN#$gD#.`iGYqlzg/GP(dN9{2rpSF3kH3L@]^`QG`X@3OYWl|GbMDRT');
+define('SECURE_AUTH_KEY',  'lv6ufGNG3UiML_>[ii[u<! $SBPSZ6XevGxtf;RwaD@JU*7,9!Y9|5*xa|0RmCw7');
+define('LOGGED_IN_KEY',    'b[se08J*Toy{RYbhxw|=.btY:~I4rsLgV2|(3<f|D,$r9-Jjzhamv*||>0tv&!+H');
+define('NONCE_KEY',        '+)jHs^+*Nj^*|5x6][)}358@T8D5:X4D=u4^N.2`8%k?ca]{+G(@**BWcsbO)GnF');
+define('AUTH_SALT',        'xSZ*pa#eo7r.lcGX))`-%ba47y!ZyMu(lu4FgX0SAuQ4E-r,[V3T%qyPc{_=F$-S');
+define('SECURE_AUTH_SALT', '|IN2A,ewYJwXtp#2*V7>G1/g>7fgr*~-WyZfc7h?-*J|l|MBSrq:9n-E)NzK f-^');
+define('LOGGED_IN_SALT',   'e*?aVE$R.rxmP|4<][||Ez2v*>mjy0ta># AF#*,X]:{pScm%gDU3y;w*uv[4_[q');
+define('NONCE_SALT',       'ELFr|!uB0D-<p;iDwyitw>Qzda-8H-zb8%|,(-:(/F-*eaJ]2k.1:~J~-|?^Qme.');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
---
+++ wp-content/themes/3a/header.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<title><?php wp_title("|", true, 'right'); bloginfo('name'); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<![endif]-->
+	<script>(function(){document.documentElement.className='js'})();</script>
+	<?php wp_enqueue_script('jquery'); ?>
+	<?php wp_head(); ?>
+
+	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css" type="text/css" media="screen" />
+
+</head>
+
+<body <?php body_class(); ?>>
