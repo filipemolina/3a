cd5076b4fe290de42f66368a8883abc6519285d0
Filipe Molina <filipemolina@live.com>
2015-01-09 18:45:46 -0200

    Imagens

---
+++ wp-content/themes/3a/sections/servicos.php
@@ -0,0 +1,57 @@
+<div class="container-fluid servicos">
+	
+	<div class="container">
+		
+		<div class="row">
+			
+			<!-- Título com a barrinha azul -->
+
+			<div class="titulo wow fadeInLeft">
+				<h2>Serviços</h2>
+				<div class="borda-azul"></div>
+			</div>
+
+			<p>3A Worldwide fornece ideias, informações e planejamento para as marcas em todo o mundo, principalmente na América do Norte,
+			América Latina e no Caribe. Com base no sul da Flórida, empregando profissionais de mídia especializados em todas as plataformas.</p>
+
+		</div>
+
+		<!-- Menu -->
+
+		<div class="row abas">
+			
+			<div class="col-sm-3">
+				<div class="item">
+					<img src="<?php echo bloginfo('template_url'); ?>/img/servicos/ico_midia.png" alt="">
+				</div>
+			</div>
+
+			<div class="col-sm-3">
+				<div class="item">
+					<img src="<?php echo bloginfo('template_url'); ?>/img/servicos/ico_relacoes_publicas.png" alt="">
+				</div>
+			</div>
+
+			<div class="col-sm-3">
+				<div class="item">
+					<img src="<?php echo bloginfo('template_url'); ?>/img/servicos/ico_digital.png" alt="">
+				</div>
+			</div>
+
+			<div class="col-sm-3">
+				<div class="item">
+					<img src="<?php echo bloginfo('template_url'); ?>/img/servicos/ico_criacao.png" alt="">
+				</div>
+			</div>
+
+		</div>
+
+		<div class="conteudo">
+			
+			
+
+		</div>
+
+	</div>
+
+</div>
