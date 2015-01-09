65e4c3598d930d715ea63b87146e0481275f2afa
Filipe Molina <filipemolina@live.com>
2015-01-09 11:51:27 -0200

    Ajustes no menu mobile

--- wp-content/themes/3a/index.php
+++ wp-content/themes/3a/index.php
@@ -1,7 +1,7 @@
 <?php get_header() ?>
 
 	<div class="container-fluid hero-unit">
-		<img src="<?php bloginfo('template_url'); ?>/img/hero-unit/frase.png" alt="" class="fadeInRight animated"/>
+		<img src="<?php bloginfo('template_url'); ?>/img/hero-unit/frase.png" alt="" class="fadeInRightBig animated"/>
 	</div>
 
 	<div class="container quem-somos">
@@ -28,9 +28,11 @@
 	<!-- Parallax 1 -->
 
 	<div class="parallax" data-stellar-background-ratio="0.5" id="parallax-1">
-		<div class="strong wow zommIn">Liberdade é a nossa inspiração</div>
+		<div class="wow zommIn strong ">Liberdade é a nossa inspiração</div>
 		<div class="wow zoomIn">para gerar resultados</div>
 	</div>
+
+	<!-- Equipe -->
 
 	<div class="container-fluid equipe">
 
@@ -99,58 +101,84 @@
 	<div class="container-fluid premios">
 
 		<div class="container">
-			<!-- Título com a barrinha azul -->
 
-			<div class="titulo wow fadeInLeft">
-				<h2>Prêmios</h2>
-				<div class="borda-azul"></div>
-			</div>	
-			<?php //Obter todos os itens da categoria "Premios" ?>
+			<div class="row">
 
-			<?php 
-				/*Argumentos de premios */
-				$args = array(
-					'post_type' => 'premio',
-					'post_status' => 'publish'
-				); 
+				<!-- Título com a barrinha azul -->
 
-				$query = new WP_Query($args);
-				$i = 0; ?>
+				<div class="titulo wow fadeInLeft">
+					<h2>Prêmios</h2>
+					<div class="borda-azul"></div>
+				</div>	
+				<?php //Obter todos os itens da categoria "Premios" ?>
 
-			<!-- Iniciar o Loop Premios -->
-			
-			<?php while($query->have_posts()) : $query->the_post();?>
+				<?php 
+					/*Argumentos de premios */
+					$args = array(
+						'post_type' => 'premio',
+						'post_status' => 'publish'
+					); 
+
+					$query = new WP_Query($args);
+					$i = 0; ?>
+
+				<!-- Iniciar o Loop Premios -->
 				
-				<?php if ($i % 4 == 0): ?>
+				<?php while($query->have_posts()) : $query->the_post();?>
+					
+					<?php if ($i % 4 == 0): ?>
 
-					<!-- Abrir uma linha à cada 4 fotos -->
+						<!-- Abrir uma linha à cada 4 fotos -->
 
-					<div class="row">
+						<div class="row">
 
-				<?php endif;?>
+					<?php endif;?>
 
-					<!-- Mostrar a Div com a foto -->
+						<!-- Mostrar a Div com a foto -->
 
-					<div class="col-md-3 container-image blocos-premios wow flipInX">
-						<img class="img-responsive" src='<?php echo types_render_field('foto', array('raw' => 'true')); ?>'/>
-						<h3><?php echo the_title(); ?></h3>
-						<?php echo the_content(); ?>
-					</div>
+						<div class="col-md-3 container-image blocos-premios wow flipInX">
+							<img class="img-responsive" src='<?php echo types_render_field('foto', array('raw' => 'true')); ?>'/>
+							<h3><?php echo the_title(); ?></h3>
+							<?php echo the_content(); ?>
+						</div>
 
 
-				<?php if (($i + 1) % 4 == 0 || $query->post_count == $i+1):  ?>
+					<?php if (($i + 1) % 4 == 0 || $query->post_count == $i+1):  ?>
 
-					<!-- Fechar uma Div à cada 4 fotos -->
+						<!-- Fechar uma Div à cada 4 fotos -->
 
-					</div>
+						</div>
 
-				<?php endif; ?>
+					<?php endif; ?>
 
-				<?php $i++; //Incrementar o contador ?>
+					<?php $i++; //Incrementar o contador ?>
 
-			<?php endwhile; ?>
+				<?php endwhile; ?>
+			</div>
+
 		</div>
 		
+	</div>
+
+	<!-- Sessão Estrutura -->
+
+	<div class="container-fluid estrutura">
+
+		<div class="container">
+
+			<div class="row">
+
+				<!-- Título com a barrinha azul -->
+
+				<div class="titulo wow fadeInLeft">
+					<h2>Estrutura</h2>
+					<div class="borda-azul"></div>
+				</div>
+
+			</div>
+
+		</div>
+
 	</div>
 
 	<!-- Parallax 2 -->
