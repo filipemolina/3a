5b5ac3dc42638cc682c9f28b53cf2b34b6f353fe
Kleiton Dias <kleitondias.ti@gmail.com>
2015-01-12 18:57:38 -0200

    cases imcompleto

---
+++ wp-content/themes/3a/sections/portfolio.php
@@ -0,0 +1,57 @@
+<!-- Portfolio -->
+
+	<div class="container-fluid portfolio">
+
+
+			<?php //Obter todos os itens da categoria "Equipe" ?>
+
+			<?php 
+				/*Argumentos de portfolio */
+				$args = array(
+					'post_type' => 'portfolio',
+					'post_status' => 'publish'
+				); 
+
+				$query = new WP_Query($args);
+				$i = 0;
+				$velocidade = 0.1; ?>
+
+			<!-- Iniciar o Loop Equipe -->
+			
+			<?php while($query->have_posts()) : $query->the_post();?>
+				
+				<?php if ($i % 4 == 0): ?>
+
+					<!-- Abrir uma linha à cada 4 fotos -->
+
+					<div class="row">
+
+				<?php endif;?>
+
+					<!-- Mostrar a Div com a foto -->
+					
+					<div class="col-md-3 container-image" data-wow-delay="<?php echo ($i+1) * $velocidade ?>s">
+
+						<div class="overlay-hover">
+
+						</div>
+
+						<img class="img-responsive zoom-image" src='<?php echo types_render_field('bg-portfolio', array('raw' => 'true')); ?>'/>
+
+					</div>
+
+
+				<?php if (($i + 1) % 4 == 0 || $query->post_count == $i+1):  ?>
+
+					<!-- Fechar uma Div à cada 4 fotos -->
+
+					</div>
+
+				<?php endif; ?>
+
+				<?php $i++; //Incrementar o contador ?>
+
+			<?php endwhile; ?>
+		
+		
+	</div>
