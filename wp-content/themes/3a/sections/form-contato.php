4b9f11874fbaba978b23106a222f60a6d1fb2891
Filipe Molina <filipemolina@live.com>
2015-01-14 19:17:25 -0200

    Formulário de COntato

--- wp-content/themes/3a/sections/form-contato.php
+++ wp-content/themes/3a/sections/form-contato.php
@@ -1,7 +1,12 @@
 <?php $pagina = get_page_by_title('Contato'); ?>
 
-<div class="container-fluid contato">
+<div class="container-fluid form-contato">
 		
 	<?php echo apply_filters("the_content", $pagina->post_content); ?>	
 
+</div>
+
+<div class="container trabalhe-conosco">
+	<p class="negrito">Trabalhe Conosco</p>
+	<p class="email"><a href="mailto:curriculum@3aworldwide.com.br">curriculum@3aworldwide.com.br</a></p>
 </div>
