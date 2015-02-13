af74fba26c3689f88d114436bdf539098ec3933a
Kleiton Dias <kleitondias.ti@gmail.com>
2015-02-13 17:54:17 -0200

    ajustes

--- wp-content/themes/3a/sections/estrutura.php
+++ wp-content/themes/3a/sections/estrutura.php
@@ -15,7 +15,8 @@
 			
 				$args = array(
 					'post_type' => 'estrutura',
-					'post_status' => 'publish'
+					'post_status' => 'publish',
+					'posts_per_page' => -1
 				); 
 
 				$query = new WP_Query($args);
